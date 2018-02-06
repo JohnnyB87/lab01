bafc6885fd6348e11cf74e1dd3a6926070be2e75
JohnnyB87 <johnbrady1987@gmail.com>
2018-02-06 19:11:15 +0000

    A complete mess 2.0

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,45 +1,107 @@
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import  javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
 public class GuessingGame {
 
-    private static int windowWidth = 300;
-    private static int windowLength = 250;
+    private static TextField txtFld;
 
-    public static VBox guessingGameTab(Button b1, Button b2, Button b3) {
+    private static Button exit;
+    private static Button reset;
+    private static Button guess;
 
-        VBox vbox = new VBox();
+    private static int numberOfGuesses = 6;
+    private static boolean winner;
+
+    public static Tab guessingGameTab(int windowWidth, int windowLength) {
+
+        Tab tab = new Tab();
+
+        tab.setText("Guessing Game");
+        tab.setContent(new Rectangle(windowWidth, windowLength, null));
+
+        String colour = "#111287";
+
+        Pane pane = new Pane();
+        pane.setBackground(new Background(new BackgroundFill(Color.web(colour),
+                CornerRadii.EMPTY, Insets.EMPTY)));
+
+        txtFld = new TextField();
+
+        exit = new Button("Exit");
+        reset = new Button("Reset");
+        guess = new Button("Guess");
+
+        txtFld.setLayoutX(75);
+        txtFld.setLayoutY(50);
+        txtFld.setAlignment(Pos.CENTER);
+
+
+        exit.setOnAction(e -> System.out.println("EXITED MOFO"));
+
+
+        TilePane tileButtons = new TilePane(Orientation.VERTICAL);
+        tileButtons.setAlignment(Pos.CENTER);
+        //tileButtons.setPadding(new Insets(50, 70, 0, 70));
+        tileButtons.setHgap(10.0);
+        tileButtons.setVgap(8.0);
+        tileButtons.setBackground(new Background(new BackgroundFill
+                (Color.web(colour),CornerRadii.EMPTY, Insets.EMPTY)));
+
+        guess.setMinWidth(tileButtons.getPrefWidth());
+        reset.setMinWidth(tileButtons.getPrefWidth());
+        exit.setMinWidth(tileButtons.getPrefWidth());
+
+        tileButtons.getChildren().addAll(txtFld, guess, reset, exit);
 
 
 
-        Tab guessingGame = new Tab();
-        guessingGame.setText("Guessing Game");
-        guessingGame.setContent(new Rectangle(windowWidth, windowLength, Color.LIGHTBLUE));
+       // pane.getChildren().addAll(guess, exit, reset);
+        tab.setContent(tileButtons);
 
 
 
-        b1 = new Button("Exit");
-        b2 = new Button("Reset");
-        b3 = new Button("Guess");
 
-        b1.setLayoutX(200);
-        b1.setLayoutY(200);
+        return tab;
+    }
 
-        vbox.getChildren().addAll(b1,b2,b3);
+    public static void run(){
+        int num = generateNumber();
+        winner = false;
+
+        try {
+            while(numberOfGuesses > 0 && !winner) {
+                guess.setOnAction(e -> Integer.parseInt(txtFld.getText()));
+
+
+                System.out.println(winner);
+
+            }
+        }
+        catch(NumberFormatException nfe){
+            System.out.println("Enter a number please.");
+        }
+
+    }
+
+    public static boolean checkGuess(int num, int guess){
+        return num == guess;
+    }
 
 
 
-        return vbox;
+    public static int generateNumber(){
+        return new Random().nextInt(100)+1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -25,17 +25,7 @@ public class Main extends Application {
 
         TabPane tabPane = new TabPane();
 
-
-
-        Button button = new Button("Test");
-        Button btn = new Button("Hello");
-
-        //TabPane guessingGame = GuessingGame.guessingGameTab(button, button, button);
-        //Tab guessingGame = GuessingGame.guessingGameTab(tabPane, button, button, button);
-
-//        Tab guessingGame = new Tab();
-//        guessingGame.setText("Guessing Game");
-//        guessingGame.setContent(new Rectangle(windowWidth,windowLength, Color.LIGHTBLUE));
+        Tab guessingGame = GuessingGame.guessingGameTab(windowWidth, windowLength);
 
         Tab lotteryGame = new Tab();
         lotteryGame.setText("Lottery Game");
@@ -45,34 +35,19 @@ public class Main extends Application {
         prizes.setText("Prizes");
         prizes.setContent(new Rectangle(windowWidth,windowLength, Color.BLUE));
 
-        //tabPane.getTabs().add(guessingGame);
+        tabPane.getTabs().add(guessingGame);
         tabPane.getTabs().add(lotteryGame);
         tabPane.getTabs().add(prizes);
 
-       // guessingGame.setContent(button);
-
-        Scene s1 = new Scene(GuessingGame.guessingGameTab(button,button,button)
-                , 300, 300);
 
         StackPane layout = new StackPane();
-        //layout.getChildren().add(guessingGame);
 
-        button.setLayoutY(200);
-        button.setLayoutX(200);
-
-        button.setLayoutY(250);
-        button.setLayoutX(220);
-
-        layout.getChildren().add(button);
-        layout.getChildren().add(btn);
-
-
-
+        layout.getChildren().add(tabPane);
 
 
         Scene scene = new Scene(layout, windowWidth, windowLength);
 
-        primaryStage.setScene(s1);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
