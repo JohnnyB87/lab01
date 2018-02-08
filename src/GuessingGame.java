cef265fb86f3b32e2d7fddbeee7a0b20f67ff936
JohnnyB87 <johnbrady1987@gmail.com>
2018-02-08 19:01:19 +0000

    1st tab working - all static methods

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -2,8 +2,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -13,31 +16,36 @@ import javafx.stage.Stage;
 
 import java.util.Random;
 
+import com.sun.glass.events.KeyEvent;
+
 public class GuessingGame {
 
-    private static TextField txtFld;
+	private static Label label = new Label();
+	private static String guessLabel = "Guesses Left: ";
 
-    private static Button exit;
-    private static Button reset;
-    private static Button guess;
-
-    private static int numberOfGuesses = 6;
-    private static boolean winner;
+    private static int guessesLeft = 6;
+    private static int num = generateNumber();
 
     public static Tab guessingGameTab(int windowWidth, int windowLength) {
 
         Tab tab = new Tab();
+        
+        Button exit;
+        Button reset;
+        Button guess;
+        TextField txtFld = new TextField();
+        
 
         tab.setText("Guessing Game");
         tab.setContent(new Rectangle(windowWidth, windowLength, null));
 
         String colour = "#111287";
+       
 
         Pane pane = new Pane();
         pane.setBackground(new Background(new BackgroundFill(Color.web(colour),
                 CornerRadii.EMPTY, Insets.EMPTY)));
 
-        txtFld = new TextField();
 
         exit = new Button("Exit");
         reset = new Button("Reset");
@@ -46,10 +54,24 @@ public class GuessingGame {
         txtFld.setLayoutX(75);
         txtFld.setLayoutY(50);
         txtFld.setAlignment(Pos.CENTER);
+        
+        label.setText(guessLabel + Integer.toString(guessesLeft));
+        label.setLayoutX(10);
+        label.setLayoutY(10);
+        label.setTextFill(Color.web("#ff0000"));
+        
 
-
-        exit.setOnAction(e -> System.out.println("EXITED MOFO"));
-
+        exit.setOnAction(e -> exitGame());
+        reset.setOnAction(e -> resetGame(txtFld));
+        guess.setOnAction(e -> run(txtFld.getText()));
+        
+        	
+        txtFld.setOnKeyPressed((event) -> { 
+        	if(event.getCode() == KeyCode.ENTER) {
+        		run(txtFld.getText());
+        		} 
+        	});
+            
 
         TilePane tileButtons = new TilePane(Orientation.VERTICAL);
         tileButtons.setAlignment(Pos.CENTER);
@@ -63,45 +85,98 @@ public class GuessingGame {
         reset.setMinWidth(tileButtons.getPrefWidth());
         exit.setMinWidth(tileButtons.getPrefWidth());
 
-        tileButtons.getChildren().addAll(txtFld, guess, reset, exit);
+        tileButtons.getChildren().addAll(label, txtFld, guess, reset, exit);
 
 
-
-       // pane.getChildren().addAll(guess, exit, reset);
         tab.setContent(tileButtons);
-
-
-
+        //tab.setContent(label);
 
         return tab;
     }
-
-    public static void run(){
-        int num = generateNumber();
-        winner = false;
-
-        try {
-            while(numberOfGuesses > 0 && !winner) {
-                guess.setOnAction(e -> Integer.parseInt(txtFld.getText()));
-
-
-                System.out.println(winner);
-
-            }
-        }
-        catch(NumberFormatException nfe){
-            System.out.println("Enter a number please.");
-        }
-
+    
+    public static void resetGame(TextField t) {
+    	num = generateNumber();
+    	guessesLeft = 6;
+    	label.setText(guessLabel + Integer.toString(guessesLeft));
+    	t.clear();
+    }
+    
+    public static void exitGame() {
+    	Alert alert = new Alert(AlertType.CONFIRMATION,
+    			"Are you sure you want to quit?",ButtonType.YES,ButtonType.NO);
+    	alert.showAndWait();
+    	
+    	if(alert.getResult() == ButtonType.YES) {
+    		Platform.exit();
+    	}
+    	else {
+    		alert.close();
+    	}
+    }
+    
+    public static boolean isNumber(String guess) {
+    	try {
+    		Integer.parseInt(guess);
+    		return true;
+    		
+    	}catch(NumberFormatException nfe){
+    		
+    		Alert alert = new Alert(Alert.AlertType.ERROR
+    				,"Enter a valid number.", ButtonType.OK);
+    		alert.showAndWait();
+    		return false;
+    	}
+    }
+    
+    public static void run(String strGuess) {
+    	
+    	System.out.println("Number: " + num);
+    	System.out.println("Lives Left: " + guessesLeft);
+    	
+    	boolean isInt = isNumber(strGuess);
+    	if(isInt) {
+    		int guess = Integer.parseInt(strGuess);
+    		checkGuess(guess);
+    		
+    	}
     }
 
-    public static boolean checkGuess(int num, int guess){
-        return num == guess;
+    public static void checkGuess(int guess){
+        boolean winner = num == guess;
+        if(winner) {
+        	prizes();
+        }
+        else {
+        	higherOrLower(guess);
+        	guessesLeft--;
+        	label.setText(guessLabel + Integer.toString(guessesLeft));
+        	Alert alert = new Alert(AlertType.INFORMATION,
+    				"You have " + guessesLeft + " guesses Left.", ButtonType.OK);
+        	alert.showAndWait();
+        }
+        
     }
 
-
+    public static void higherOrLower(int guess) {
+    	Alert alert = new Alert(AlertType.INFORMATION, "", ButtonType.OK);
+    	
+    	if(guess > num)
+    		alert.setContentText("You guessed too high.");
+    	else
+    		alert.setContentText("You guessed too low.");
+    	
+    	alert.showAndWait();
+    }
+    
+    public static void prizes() {
+    	int num = guessesLeft > 4 ? 4 : guessesLeft;
+    	Alert alert = new Alert(AlertType.CONFIRMATION,
+				"You won a " + num + " star prize.", ButtonType.OK);
+    	alert.showAndWait();
+    }
 
     public static int generateNumber(){
         return new Random().nextInt(100)+1;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     private int windowWidth = 300;
-    private int windowLength = 250;
+    private int windowHeight = 300;
 
     public static void main(String[] args){
         launch(args);
@@ -22,18 +22,22 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Window");
+        primaryStage.setMaxHeight(windowHeight);
+        primaryStage.setMaxWidth(windowWidth);
+        primaryStage.setMinHeight(windowHeight);
+        primaryStage.setMinWidth(windowWidth);
 
         TabPane tabPane = new TabPane();
 
-        Tab guessingGame = GuessingGame.guessingGameTab(windowWidth, windowLength);
+        Tab guessingGame = GuessingGame.guessingGameTab(windowWidth, windowHeight);
 
         Tab lotteryGame = new Tab();
         lotteryGame.setText("Lottery Game");
-        lotteryGame.setContent(new Rectangle(windowWidth,windowLength, Color.LAVENDER));
+        lotteryGame.setContent(new Rectangle(windowWidth,windowHeight, Color.LAVENDER));
 
         Tab prizes = new Tab();
         prizes.setText("Prizes");
-        prizes.setContent(new Rectangle(windowWidth,windowLength, Color.BLUE));
+        prizes.setContent(new Rectangle(windowWidth,windowHeight, Color.BLUE));
 
         tabPane.getTabs().add(guessingGame);
         tabPane.getTabs().add(lotteryGame);
@@ -45,7 +49,7 @@ public class Main extends Application {
         layout.getChildren().add(tabPane);
 
 
-        Scene scene = new Scene(layout, windowWidth, windowLength);
+        Scene scene = new Scene(layout, windowWidth, windowHeight);
 
         primaryStage.setScene(scene);
         primaryStage.show();
