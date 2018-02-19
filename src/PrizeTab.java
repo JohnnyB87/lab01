928a71a2e5607b2d34be8e45438d59c0e99967ca
JohnnyB87 <johnbrady1987@gmail.com>
2018-02-19 18:54:25 +0000

    added buttons to prize tab

--- src/PrizeTab.java
+++ src/PrizeTab.java
@@ -1,9 +1,13 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -17,21 +21,35 @@ import java.util.Iterator;
 
 public class PrizeTab extends Tab{
 //    HashMap<String, HashMap<Integer, String>> prizes = new HashMap();
-    HashMap<String, String> prizes;
+    private HashMap<String, String> prizes;
+    private TilePane tileButtons;
+    private Button[] buttons;
 
 
     public PrizeTab() {
         this.setText("Prizes");
-        //this.setContent(new Rectangle(windowWidth,windowHeight, Color.BLUE));
-       // loadPrizes(5);
+
+        tileButtons = new TilePane(Orientation.VERTICAL);
+        tileButtons.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        tileButtons.setAlignment(Pos.CENTER);
+        tileButtons.setHgap(5.0);
+        tileButtons.setVgap(8.0);
+        tileButtons.setMinHeight(75);
+
+
+        this.setContent(this.tileButtons);
+
     }
 
     public void loadPrizes(int n){
         try{
             this.prizes = new HashMap();
-            File readMe = new File("prizes.txt");
+            String pathToFile = "prizes.txt";
+
+            File readMe = new File(pathToFile);
             FileReader fr = new FileReader(readMe);
             BufferedReader br = new BufferedReader(fr);
+
             String newLine = br.readLine();
             do{
                 String[] array = newLine.split(",");
@@ -50,17 +68,28 @@ public class PrizeTab extends Tab{
         }catch(IOException ioe){
 
         }
+        buttons = new Button[this.prizes.size()];
 
     }
 
     public void showPrizes(){
-        String str = "";
 
-        for(HashMap.Entry<String,String> e : this.prizes.entrySet()){
-            str += e.getValue() + "\n";
+        int i = 0;
+        for(HashMap.Entry<String,String> entry : this.prizes.entrySet()){
+            this.buttons[i] = new Button(entry.getValue());
+            //str += e.getValue() + "\n";
+            this.tileButtons.getChildren().add(this.buttons[i]);
+            int j = i;
+            this.buttons[i].setOnAction(e -> {
+                String str = String.format("You chose the %s prize.%n", this.buttons[j].getText());
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, str);
+                alert.showAndWait();
+            });
+
+            i++;
         }
 
-        Alert alert = new Alert(Alert.AlertType.INFORMATION, str);
-        alert.showAndWait();
+//        Alert alert = new Alert(Alert.AlertType.INFORMATION, str);
+//        alert.showAndWait();
     }
 }
